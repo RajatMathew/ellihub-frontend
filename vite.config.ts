73577b2af18c8f880aa6c7babf390fec426a3020
mohammed-yasim <mohammedyasim.edkm@gmail.com>
2026-01-29 15:14:15 +0530

    feat: initialize project with Vite, React, and TypeScript setup
    - Add ESLint configuration for TypeScript and React
    - Create index.html as the entry point
    - Set up package.json with dependencies and scripts for development, build, and linting
    - Include Vite logo SVG in public directory
    - Implement basic App component in index.tsx
    - Add Tailwind CSS import in main.css
    - Set up main.tsx for rendering the App component
    - Configure TypeScript with app and node configurations
    - Create Vite configuration with React, Tailwind CSS, and PWA support

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Elle Hub",
+        short_name: "Elle",
+        description: "construction company erp",
+        theme_color: "#ffffff",
+      },
+    }),
+  ],
+});
